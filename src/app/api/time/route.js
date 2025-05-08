9e8a3e40bbf650255cec2189466f9a120f5de249
なえざつく <naezatsuku653@gmail.com>
2025-05-09 00:54:08 +0900

    5/9動くか確かめる

--- src/app/api/time/route.js
+++ src/app/api/time/route.js
@@ -1,9 +1,16 @@
 import { NextResponse } from "next/server";
-//import { PrismaClient } from "@/prisma/client";
-//const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export async function POST(req) {
-    const data = await req.json();
-    console.log(data);
-    return NextResponse.json({message:"ok"});
+    const datas = await req.json();
+    console.log(datas);
+    for(let i=0;i<datas.length;i++ ){
+       const data = datas[i];
+       const {className , time } = data 
+       await prisma.outline.create({
+        data:{className:className,time:time}
+       });
+    }
+    return NextResponse.json(datas);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
+"use client"
 export default function Home(){
+    async function addData(){
+        const res = await fetch("/api/time",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify([{className:"１年",time:100},{className:"二年",time:100}])
+        });
+       console.log(res);
+    }
     return(
         <>
         hello
+        <button onClick={addData}>add data</button>
         </>
     )
 
