c14979f0668bfc67f4bcca97218bea5d99fa71be
なえざつく <naezatsuku653@gmail.com>
2025-05-09 01:19:46 +0900

    again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        allowedDevOrigins: ["http://192.168.1.103:3000", "http://localhost:3000"]
+        
       }
     
 
